91c0857da80a89f3be52061e553308339b5664a3
sy <somarl@live.com>
2021-12-27 18:39:59 +0800

    chore(page): add favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,20 @@ export default class MyDocument extends Document {
     public render () {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <title>Apple Music Analyser</title>
+                    <meta name="description" content="Apple Music activities visualization." />
+
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fa243c" />
+                    <meta name="msapplication-TileColor" content="#b91d47" />
+                    <meta name="theme-color" content="#fa243c" />
+
+                    <meta name="robots" content="all" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
